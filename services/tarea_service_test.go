1acbd7f41c7d66b467a18adb907cf098da273aa8
juanma <arenasrinconj@gmail.com>
2025-04-03 19:42:56 -0500

    Test de creación de tarea con entorno de desarrollo

---
+++ services/tarea_service_test.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+	"github.com/MoonHack2077/Parcial2-SO/config"
+	"github.com/MoonHack2077/Parcial2-SO/models"
+)
+
+func TestCrearTarea(t *testing.T) {
+	// Cargar .env
+	err := godotenv.Load("../.env")
+	if err != nil {
+		t.Fatal("❌ No se pudo cargar el archivo .env")
+	}
+
+	// Verificar que la URI esté bien formada
+	if os.Getenv("MONGO_URI_TEST") == "" {
+		t.Fatal("❌ MONGO_URI está vacío o no cargado")
+	}
+	
+	config.ConectarDB() // ← ESTA LÍNEA INICIALIZA LA CONEXIÓN
+
+	tarea := models.Tarea{
+		Titulo:      "Prueba",
+		Descripcion: "Esto es una prueba",
+		Completado:  false,
+	}
+
+	id, err := CrearTarea(tarea)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, id)
+}
